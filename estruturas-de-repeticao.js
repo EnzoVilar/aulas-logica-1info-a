eade2438947caff1da963857795053624cb60388
EnzoVilar <enzovilarpires@gmail.com>
2026-03-23 10:20:36 -0300

    Foi feito uma tabuada

---
+++ estruturas-de-repeticao.js
@@ -0,0 +1,13 @@
+//while, do while, for
+
+console.log("saí do while")
+
+let numero = 6
+let tabuada = 0
+let resultado = 0
+
+while (resultado <= numero * 10) {
+    console.log(tabuada)
+    tabuada = tabuada + 1
+    resultado = tabuada * numero
+}
